f29094cf5fd016aa26772d44fe7eea35bf820eda
Shynma <105978008+Shynma@users.noreply.github.com>
2022-05-21 16:12:43 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -2,13 +2,13 @@
 #            Fonctions des onglets           #
 ##############################################
 def intro():
-
+  st.markdown("page intro")
 
 def globale():
-
+  st.markdown("page globale")
 
 def indiv():
-
+  st.markdown("page ind")
 
 def modelisation():
-
+  st.markdown("page modelisation")
