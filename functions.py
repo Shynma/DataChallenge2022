b61796c5fb1e6e951648bcc765eb190bd6dc2c0b
Shynma <105978008+Shynma@users.noreply.github.com>
2022-05-22 20:08:46 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -13,6 +13,15 @@ def intro():
 def projet():
   st.image("images/Data4good.png", use_column_width=True)
   st.image("images/Data4good2.png", use_column_width=True)
+  
+  st.markdown("# Petite histoire du son")
+  col1,col2 = st.columns((1,3))
+  col1.markdown(
+    Deux caractéristiques du son :
+    -	La ***fréquence*** (nombre d’oscillations par seconde) 
+    -	L’***amplitude*** (intensité des oscillations)
+  )
+  col2.image("https://docplayer.fr/docs-images/17/98421/images/3-0.png", width = 400)
 
 def use_case():
   st.image("images/banniere_fixe.png", use_column_width=True)
