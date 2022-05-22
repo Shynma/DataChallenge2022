f64abfb41fc9416b1245160e60fc1a5f18750f81
Shynma <105978008+Shynma@users.noreply.github.com>
2022-05-22 20:18:20 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import numpy as np
+import pandas as pd
+import plotly.express as px
 
 ##############################################
 #            Fonctions des onglets           #
