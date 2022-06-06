90ae06e95feb85442baf91835d2ac39cbbfe580e
Shynma <105978008+Shynma@users.noreply.github.com>
2022-06-06 09:48:41 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -241,6 +241,7 @@ def projet():
     p = apply_model(samples, mpath)
     resultat = model_output(p)
     st.write("##### PRÉDICTION")
+    st.image("images/fleche.png", use_column_width=True)
     st.markdown(resultat,unsafe_allow_html=True)
   
 #   uploaded_file = st.file_uploader("Choisir un fichier audio à analyser", type = ["WAV", "AIF", "MP3", "MID"])
