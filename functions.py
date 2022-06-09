b6155645b05690dfdb83954ebed29e530440daaf
Shynma <105978008+Shynma@users.noreply.github.com>
2022-06-09 09:32:13 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -26,7 +26,7 @@ height_carac = 300
 ##############################################
 def intro():
   st.image("images/banniere_vivatech.png", use_column_width=True)
-  st.image("images/epsilon0.png", use_column_width=True)
+  st.image("images/epsilon0.png")
   st.markdown('#')
   st.markdown('#')
   st.markdown('#')
@@ -34,8 +34,8 @@ def intro():
 
 def projet():
   st.image("images/banniere_vivatech.png", use_column_width=True)
-  st.image("images/projet0.png", use_column_width=True)
-  st.image("images/projet1.png", use_column_width=True)
+  st.image("images/projet0.png")
+  st.image("images/projet1.png")
   
   st.markdown(
     """
@@ -251,7 +251,7 @@ def projet():
   
 def use_case():
   st.image("images/banniere_vivatech.png", use_column_width=True)
-  st.image("images/use_case_final.png", use_column_width=True)
+  st.image("images/use_case_final.png")
   st.image("images/merci.png")
 
   
