6576e5f98ef5c6e55bf93e9a7dffebde75ca117b
Shynma <105978008+Shynma@users.noreply.github.com>
2022-06-10 22:07:20 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,8 +36,8 @@ if __name__ == "__main__":
         <style>
         @font-face {
             font-family: 'trashbold';
-            src: url('../../DataChallenge2022/blob/master/trash-bold-webfont.woff2') format('woff2'),
-                 url('../../DataChallenge2022/blob/master/trash-bold-webfont.woff') format('woff');
+            src: url('/trash-bold-webfont.woff2') format('woff2'),
+                 url('/trash-bold-webfont.woff') format('woff');
             font-weight: normal;
             font-style: normal;
 
