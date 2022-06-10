38a6ccc518c3a0770d52cafd33bf9065184539af
Shynma <105978008+Shynma@users.noreply.github.com>
2022-06-10 19:37:18 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,6 +18,11 @@ DEMOS = OrderedDict(
 
 # Fonctionnement de l'application
 def run():
+    with open('style.css') as f:
+        st.markdown(
+            f'(<style>{f.read()}</style>'
+            , unsafe_allow_html = True
+        )
     st.set_page_config(page_title="Vivatch - Epsilon", page_icon="https://img.icons8.com/color/48/000000/stork.png", layout="wide", initial_sidebar_state="auto", menu_items=None)
     #demo_name = st.sidebar.selectbox("Menu", list(DEMOS.keys()), 0)
     #demo = DEMOS[demo_name]
